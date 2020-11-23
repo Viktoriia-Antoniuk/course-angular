f3a1834d1d82febc2bab251598c204ec79b71e62
Viktoriia-Antoniuk <vika82134@gmail.com>
2020-11-23 22:48:07 +0200

    homework1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angularHome1';
+  tasks = [{
+    name: 'Task 1',
+    isDone: true
+  },
+  {
+    name: 'Task 2',
+    isDone: false
+  },
+  {
+    name: 'Task 3',
+    isDone: true
+  },
+  {
+    name: 'Task 4',
+    isDone: true
+  },
+  {
+    name: 'Task 5',
+    isDone: false
+  }];
 }
