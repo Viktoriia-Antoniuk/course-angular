2a0d39b4a70971916655e9a23b7b6589fb18973a
Viktoriia-Antoniuk <vika82134@gmail.com>
2020-11-23 17:06:44 +0200

    lesson1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  
+  name = 'Vika';
+
+  number = 10;
+
+  arrNumber = [10, 40, 49];
+
+  products = [{
+    name: 'Product 1',
+    price: 20,
+    isStock: true
+  },
+  {
+    name: 'Product 2',
+    price: 10,
+    isStock: false
+  }];
+
+  isOpenMenu = false;
+
+  openDropdown() {
+    this.isOpenMenu = !this.isOpenMenu;
+  }
 }
