09e6e7acd7bcb586c8a62c1ec003029c10d568e2
Viktoriia-Antoniuk <vika82134@gmail.com>
2020-12-27 16:52:19 +0200

    Forms

---
+++ src/app/form/form.component.ts
@@ -0,0 +1,51 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.scss']
+})
+export class FormComponent implements OnInit {
+  formReg: FormGroup;
+  
+  get form() {
+    return this.formReg.controls;
+  }
+
+  @Input() formType: string = 'registration';
+  
+  login = {
+    email: '',
+    password: '',
+  };
+
+
+  constructor(
+    private fb: FormBuilder
+  ) {}
+
+  ngOnInit(): void {
+    this.formReg = this.fb.group({
+      name: ['', [Validators.required]],
+      fullname: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.pattern('^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$')]],
+      confirm: ['', [Validators.required]]
+    }, this.pwdMatchValidator);
+  }
+  pwdMatchValidator(group: FormGroup) {
+    let pass = this.formReg.controls.password.value;
+    let confirmPass = this.formReg.controls.confirnm.value;
+
+    return pass === confirmPass ? null : { notSame: true };
+ }
+
+  onReg() {
+    console.log(this.formReg.value);
+  }
+
+  onLog() {
+    console.log(this.login);
+  }
+}
