255961f4b2943590f8b8a375fd2baff8122b340a
Viktoriia-Antoniuk <vika82134@gmail.com>
2021-01-08 16:50:00 +0200

    view

--- src/app/todo-single/todo-single.module.ts
+++ src/app/todo-single/todo-single.module.ts
@@ -3,13 +3,20 @@ import { CommonModule } from '@angular/common';
 
 import { TodoSingleRoutingModule } from './todo-single-routing.module';
 import { TodoSingleComponent } from './todo-single/todo-single.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
+
+
 
 
 @NgModule({
   declarations: [TodoSingleComponent],
   imports: [
     CommonModule,
-    TodoSingleRoutingModule
+    TodoSingleRoutingModule,
+    MatButtonModule,
+    MatIconModule
   ]
 })
 export class TodoSingleModule { }
--- src/app/todo-single/todo-single/todo-single.component.ts
+++ src/app/todo-single/todo-single/todo-single.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-todo-single',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodoSingleComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
   }
 
+  backTo() {
+    this.router.navigate(['todo']);
+  }
 }
